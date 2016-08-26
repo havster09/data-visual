f16002245a03b195324803dce5bf89f23d843ccc
havster09 <havster09@gmail.com>
2016-08-27 09:46:32 +1000

    realtime nvd3

--- scripts/components/dashboard-app.js
+++ scripts/components/dashboard-app.js
@@ -8,7 +8,10 @@ dashboard.component("dashboardApp", {
         {path:"/bar-graph", component: "barGraph", name: "Bar Graph"},
         {path:"/about", component: "about", name: "About"},
         {path:"/d3-bar-chart", component: "d3BarChart", name: "D3 Bar Chart"},
+
         {path:"/nvd3-quickstart", component: "nvd3Quickstart", name: "NvD3 Quickstart"},
+        {path:"/nvd3-realtime", component: "nvd3Realtime", name: "NvD3 Realtime"},
+
         {path:"/nvd3-directives", component: "nvd3Directives", name: "NvD3 Directives"},
         {path:"/**", redirectTo:["List"]}
     ]
---
+++ scripts/components/nvd3-realtime.js
@@ -0,0 +1,78 @@
+dashboard.component("nvd3Realtime", {
+    templateUrl:"scripts/components/nvd3-realtime.html",
+    controller: function($scope) {
+        var model = $scope;
+        model.message = "you gonna have a heart attack";
+
+        model.colorFunction = function() {
+            return function(d, i) {
+                return '#E01B5D'
+            };
+        };
+
+
+        model.options = {
+            chart: {
+                type:'lineChart',
+                height: 400,
+                margin: {
+                    top:20,
+                    right: 20,
+                    bottom: 40,
+                    left: 55
+                },
+                x: function(d) {
+                    return d.x;
+                },
+                y: function(d) {
+                    return d.y;
+                },
+                userInteractiveGuideline:true,
+                duration: 500,
+                yAxis:{
+                    tickFormat: function(d) {
+                        return d3.format('.01f')(d);
+                    }
+                },
+                color: function(d,i) {
+                    /*var colors = d3.scale.category20().range();
+                    var rnd = Math.floor(Math.random() * colors.length);
+                    return colors[rnd];*/
+
+                    var yVal = d.values[d.values.length-1].y;
+                    console.log(yVal);
+                    return (yVal < 0)?"red":"green";
+                }
+            }
+        };
+
+        model.data = [
+            {
+                values:[],key:'My Profits'
+            }
+        ];
+
+        model.run = true;
+
+        var x = 0;
+
+        setInterval(function() {
+            if(!model.run) {
+                return;
+            }
+
+            function getProfit() {
+                var winLose = ((Math.random() * 10) < 5)? -1 : 1;
+                var profit = Math.random() * 100 * (Math.random() * 1 * Math.random() * winLose);
+                return profit;
+            }
+
+            model.data[0].values.push({x:x, y: getProfit()});
+            if(model.data[0].values.length > 20) {
+                model.data[0].values.shift();
+            }
+            x++;
+            $scope.$apply();
+        }, 500)
+    }
+});
